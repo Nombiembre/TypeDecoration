e320882fd0ae05ae38adce57c418a8a025a63fe4
Nombiembre <135085935+Nombiembre@users.noreply.github.com>
2024-10-31 09:33:10 -0500

    Delete .vercel/output directory

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/en_D6AP_9FJ.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./en_BBT_q9YI.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
